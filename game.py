9025fe0a067c40cfbea7f82e529acb4a038abe88
whiteAsh <w8ithash@gmail.com>
2026-04-09 14:08:07 +0700

    add lvl system

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ from enemy import random_enemy
 from player import Player
 from map import generate_map
 from battle import battle
-from render import render_all
+from render import render_all, draw_map
 from title import draw_title_screen
 
 player = Player()
@@ -20,43 +20,48 @@ def main(stdscr):
     stdscr.getch()
 
     while player.is_alive():
-        render_all(stdscr, game_map, player)
+        # render_all(stdscr, game_map, player)
+        stdscr.clear()
+        draw_map(stdscr, game_map, player)
 
         key = stdscr.getch()
+
+        moved = False
 
         if key == ord("q"):
             break
 
         elif key == curses.KEY_UP:
-            player.move(0, -1, game_map)
+            moved = player.move(0, -1, game_map)
         elif key == curses.KEY_DOWN:
-            player.move(0, 1, game_map)
+            moved = player.move(0, 1, game_map)
         elif key == curses.KEY_LEFT:
-            player.move(-1, 0, game_map)
+            moved = player.move(-1, 0, game_map)
         elif key == curses.KEY_RIGHT:
-            player.move(1, 0, game_map)
+            moved = player.move(1, 0, game_map)
 
-        if random.random() < 0.05:
-            enemy = random_enemy()
+        if moved:
+            if random.random() < 0.05:
+                enemy = random_enemy()
 
-            result = battle(stdscr, player, enemy)
-            if result == "win":
-                stdscr.clear()
-                stdscr.addstr(10, 2, f"{enemy.name} defeated!")
-                stdscr.refresh()
-                stdscr.getch()
-            elif result == "lose":
-                stdscr.clear()
-                stdscr.addstr(10, 2, "KoPoon was defeated...")
-                stdscr.refresh()
-                stdscr.getch()
-            elif result == "run":
-                stdscr.clear()
-                stdscr.addstr(10, 2, "You escaped!")
-                stdscr.refresh()
-                stdscr.getch()
-            elif result == "quit":
-                break
+                result = battle(stdscr, player, enemy)
+                if result == "win":
+                    stdscr.clear()
+                    stdscr.addstr(10, 2, f"{enemy.name} defeated!")
+                    stdscr.refresh()
+                    stdscr.getch()
+                elif result == "lose":
+                    stdscr.clear()
+                    stdscr.addstr(10, 2, "KoPoon was defeated...")
+                    stdscr.refresh()
+                    stdscr.getch()
+                elif result == "run":
+                    stdscr.clear()
+                    stdscr.addstr(10, 2, "You escaped!")
+                    stdscr.refresh()
+                    stdscr.getch()
+                elif result == "quit":
+                    break
 
 
 curses.wrapper(main)
--- player.py
+++ player.py
@@ -2,18 +2,41 @@ class Player:
     def __init__(self):
         self.x = 2
         self.y = 2
-        self.hp = 100
+
+        self.lvl = 1
+        self.exp = 0
+        self.exp_to_next = 10
+
+        self.max_hp = 100
+        self.hp = self.max_hp
         self.attack = 20
 
+    # handle the player movement
     def move(self, dx, dy, map):
         new_x = self.x + dx
         new_y = self.y + dy
 
+        # check for collision
         if map[new_y][new_x] == "#":
-            return
-
+            return False
         self.x = new_x
         self.y = new_y
+        return True
+
+    # exp gain
+    def gain_exp(self, amount):
+        self.exp += amount
+        while self.exp >= self.exp_to_next:
+            self.lvl_up()
+
+    # leveling system
+    def lvl_up(self):
+        self.lvl += 1
+        self.exp -= self.exp_to_next
+        self.exp_to_next = int(self.exp_to_next * 1.5)
+
+        self.max_hp += 5
+        self.attack += 1
 
     def take_damage(self, damage):
         self.hp -= damage
